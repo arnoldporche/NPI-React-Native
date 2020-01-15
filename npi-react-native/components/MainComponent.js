81ee5edc58d595abbae6c78da15dcc41ab9dcbb7
Administrator <administrator@Administrators-MacBook-Pro.local>
2020-01-14 16:10:04 -0800

    adimplemented react-navigation

---
+++ npi-react-native/components/MainComponent.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import Directory from './DirectoryComponent';
+import CampsiteInfo from './CampsiteInfoComponent';
+import { View, Platform } from 'react-native';
+import { createStackNavigator } from 'react-navigation';
+
+// Not t display warning issues (yellow) within the app - once parts are updated, then I can remove this for production
+console.disableYellowBox = true;
+
+const DirectoryNavigator = createStackNavigator(
+    {
+        Directory: { screen: Directory },
+        CampsiteInfo: { screen: CampsiteInfo }
+    }, 
+    {
+        initialRouteName: 'Directory',
+        navigationOptions: {
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
+        }
+    }
+);
+
+class Main extends Component {
+    render() {
+        return (
+            <View style={{flex: 1, paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight }}>
+                <DirectoryNavigator />
+            </View>
+        );
+    }
+}
+
+export default Main;
