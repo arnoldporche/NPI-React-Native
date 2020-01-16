ca167e18c86ff92361b7a8e1013b743370d2c397
arnoldporche <arnoldporche@yahoo.com>
2020-01-15 21:48:49 -0800

    updated nav bar to smaller height, started working on Individual list

---
+++ npi-react-native/components/IndividualComponent.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import { FlatList } from "react-native";
+import { ListItem } from "react-native-elements";
+import { CAMPSITES } from "../shared/campsites";
+
+class Individual extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      campsites: CAMPSITES
+    };
+  }
+
+  static navigationOptions = {
+    title: "Individual"
+  };
+
+  render() {
+    const { navigate } = this.props.navigation;
+    const renderIndividualItem = ({ item }) => {
+      return (
+        <ListItem
+          title={item.name}
+          subtitle={item.description}
+          onPress={() => navigate("CampsiteInfo", { campsiteId: item.id })}
+          leftAvatar={{ source: require("./images/react-lake.jpg") }}
+        />
+      );
+    };
+
+    return (
+      <FlatList
+        data={this.state.campsites}
+        renderItem={renderIndividualItem}
+        keyExtractor={item => item.id.toString()}
+      />
+    );
+  }
+}
+
+export default Individual;
--- npi-react-native/components/MainComponent.js
+++ npi-react-native/components/MainComponent.js
@@ -1,76 +1,80 @@
-import React, { Component } from 'react';
-import Home from './HomeComponent';
-import Directory from './DirectoryComponent';
-import CampsiteInfo from './CampsiteInfoComponent';
-import ActionBarImage from './ActionBarImage'
-import { View, Platform, Image } from 'react-native';
-import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
+import React, { Component } from "react";
+import Home from "./HomeComponent";
+import Individual from "./IndividualComponent";
+import CampsiteInfo from "./CampsiteInfoComponent";
+import ActionBarImage from "./ActionBarImage";
+import { View, Platform, Image } from "react-native";
+import { createStackNavigator, createDrawerNavigator } from "react-navigation";
 
 // Not t display warning issues (yellow) within the app - once parts are updated, then I can remove this for production
 console.disableYellowBox = true;
 
-const DirectoryNavigator = createStackNavigator(
-    {
-        Directory: { screen: Directory },
-        CampsiteInfo: { screen: CampsiteInfo }
-    },
-    {
-        initialRouteName: 'Directory',
-        navigationOptions: {
-            headerStyle: {
-                backgroundColor: '#01579B'
-            },
-            headerTintColor: '#fff',
-            headerTitleStyle: {
-                color: '#fff'
-            }
-        }
+const IndividualNavigator = createStackNavigator(
+  {
+    Individual: { screen: Individual },
+    CampsiteInfo: { screen: CampsiteInfo }
+  },
+  {
+    initialRouteName: "Individual",
+    navigationOptions: {
+      headerLeft: <ActionBarImage />,
+      headerStyle: {
+        height: 20,
+        backgroundColor: "#01579B"
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: {
+        color: "#fff"
+      }
     }
+  }
 );
 
 const HomeNavigator = createStackNavigator(
-    {
-        Home: { screen: Home }
-    },
-    {
-        navigationOptions: {
-            headerLeft: <ActionBarImage />,
-            headerStyle: {
-                height: 100,
-                backgroundColor: '#01579B'
-            },
-            headerTintColor: '#fff',
-            headerTitleStyle: {
-                color: '#fff'
-            }
-        }
+  {
+    Home: { screen: Home }
+  },
+  {
+    navigationOptions: {
+      headerLeft: <ActionBarImage />,
+      headerStyle: {
+        height: 20,
+        backgroundColor: "#01579B"
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: {
+        color: "#fff"
+      }
     }
+  }
 );
 
 const MainNavigator = createDrawerNavigator(
-    {
-        Home: { screen: HomeNavigator },
-        Directory: { screen: DirectoryNavigator }
-    },
-    {
-        drawerBackgroundColor: '#01579B',
-        labelStyle: {
-            color: '#fff',
-        },
+  {
+    Home: { screen: HomeNavigator },
+    Individual: { screen: IndividualNavigator }
+  },
+  {
+    drawerBackgroundColor: "#01579B",
+    labelStyle: {
+      color: "#fff"
     }
+  }
 );
 
 class Main extends Component {
-    render() {
-        return (
-            <View style={{
-                flex: 1,
-                paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight
-            }}>
-                <MainNavigator />
-            </View>
-        );
-    }
+  render() {
+    return (
+      <View
+        style={{
+          flex: 1,
+          paddingTop: Platform.OS === "ios" ? 0 : Expo.Constants.statusBarHeight
+        }}
+      >
+        <MainNavigator />
+      </View>
+    );
+  }
 }
 
 export default Main;
