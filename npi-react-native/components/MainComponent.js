c269bca7b48c8903650fdbec2621333f6d428e7d
arnoldporche <arnoldporche@yahoo.com>
2020-01-17 23:23:41 -0800

    updated the drawer navigation

--- npi-react-native/components/MainComponent.js
+++ npi-react-native/components/MainComponent.js
@@ -3,8 +3,21 @@ import Home from "./HomeComponent";
 import Individual from "./IndividualComponent";
 import IndividualInfo from "./IndividualInfoComponent";
 import ActionBarImage from "./ActionBarImage";
-import { View, Platform } from "react-native";
-import { createStackNavigator, createDrawerNavigator } from "react-navigation";
+import {
+  View,
+  Platform,
+  StyleSheet,
+  Text,
+  ScrollView,
+  Image
+} from "react-native";
+import { Icon } from "react-native-elements";
+import {
+  createStackNavigator,
+  createDrawerNavigator,
+  DrawerItems
+} from "react-navigation";
+import SafeAreaView from "react-native-safe-area-view";
 
 // Not t display warning issues (yellow) within the app - once parts are updated, then I can remove this for production
 console.disableYellowBox = true;
@@ -49,13 +62,54 @@ const HomeNavigator = createStackNavigator(
   }
 );
 
+const CustomDrawerContentComponent = props => (
+  <ScrollView>
+    <SafeAreaView
+      style={styles.container}
+      forceInset={{ top: "always", horizontal: "never" }}
+    >
+      <View style={styles.drawerHeader}>
+        <View style={{ flex: 1 }}>
+          <Image
+            source={{
+              uri:
+                "https://s3-us-west-1.amazonaws.com/fraymework/multimedia/images/icons/apps/npicheck.png"
+            }}
+            style={styles.drawerImage}
+          />
+        </View>
+        <View style={{ flex: 2 }}>
+          <Text style={styles.drawerHeaderText}>NPI</Text>
+          <Text>National Provider Identifier</Text>
+        </View>
+      </View>
+      <DrawerItems {...props} />
+    </SafeAreaView>
+  </ScrollView>
+);
+
 const MainNavigator = createDrawerNavigator(
   {
-    Home: { screen: HomeNavigator },
-    Individual: { screen: IndividualNavigator }
+    Home: {
+      screen: HomeNavigator,
+      navigationOptions: {
+        drawerIcon: ({ tintColor }) => (
+          <Icon name="home" type="font-awesome" size={24} color={tintColor} />
+        )
+      }
+    },
+    Individual: {
+      screen: IndividualNavigator,
+      navigationOptions: {
+        drawerIcon: ({ tintColor }) => (
+          <Icon name="list" type="font-awesome" size={24} color={tintColor} />
+        )
+      }
+    }
   },
   {
     drawerBackgroundColor: "#64B5F6",
+    contentComponent: CustomDrawerContentComponent,
     labelStyle: {
       color: "#fff"
     }
@@ -76,5 +130,34 @@ class Main extends Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  drawerHeader: {
+    backgroundColor: "#1565C0",
+    height: 140,
+    alignItems: "center",
+    justifyContent: "center",
+    flex: 1,
+    flexDirection: "row"
+  },
+  drawerHeaderText: {
+    color: "#fff",
+    fontSize: 24,
+    fontWeight: "bold"
+  },
+  drawerImage: {
+    margin: 10,
+    height: 60,
+    width: 60
+  },
+  stackIcon: {
+    marginLeft: 10,
+    color: "#fff",
+    fontSize: 24
+  }
+});
 
 export default Main;
